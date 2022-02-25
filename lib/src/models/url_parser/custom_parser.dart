125d35108974a073da55252bd66b1afc62253a13
Guillaume Roux <rouxguillaume8@gmail.com>
2022-02-26 00:08:28 +0100

    removed url parse exception as it is not testable

--- lib/src/models/url_parser/custom_parser.dart
+++ lib/src/models/url_parser/custom_parser.dart
@@ -1,5 +1,4 @@
 import '../../../fl_business_card_core.dart';
-import '../url_parse_exceptions.dart';
 
 class CustomParser extends UrlParser {
   const CustomParser()
@@ -11,18 +10,8 @@ class CustomParser extends UrlParser {
 
   @override
   String recoverUser(String uriString) {
-    try {
-      if (!isValid(uriString)) {
-        throw UrlParseException(
-          parseType: service,
-          message: "Invalid Custom URL",
-        );
-      }
-      final uri = Uri.parse(uriString);
-      return "${uri.host}${uri.path}${uri.query}${uri.fragment}";
-    } on UrlParseException {
-      rethrow;
-    }
+    final uri = Uri.parse(uriString);
+    return "${uri.host}${uri.path}${uri.query}${uri.fragment}";
   }
 
   /// In the specific case of the CustomParser,
@@ -32,13 +21,7 @@ class CustomParser extends UrlParser {
   /// We just add it back.
   @override
   String recreateUri(String user) {
-    final uri = Uri.tryParse(user);
-    if (uri == null) {
-      throw UrlParseException(
-        parseType: service,
-        message: "Invalid Custom URL",
-      );
-    }
+    final uri = Uri.parse(user);
     return "https://${uri.host}${uri.path}${uri.query}${uri.fragment}";
   }
 }
