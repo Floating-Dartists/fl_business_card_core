3b5d2210e100e75cb7d72a2e3835e213ee44ca4f
Guillaume Roux <rouxguillaume8@gmail.com>
2022-02-25 22:33:29 +0100

    unit tested UrlParseException

--- test/parser/url_parser_test.dart
+++ test/parser/url_parser_test.dart
@@ -1,4 +1,5 @@
 import 'package:fl_business_card_core/fl_business_card_core.dart';
+import 'package:fl_business_card_core/src/models/url_parse_exceptions.dart';
 import 'package:test/test.dart';
 
 const _tService = 'a';
@@ -15,12 +16,24 @@ class MockParserB extends UrlParser {
 void main() {
   group('UrlParser', () {
     group('props', () {
-      test('2 objects with same props are equal', () {
-        const a = MockParserA();
-        const b = MockParserA();
+      const tParserA = MockParserA();
+      const tParserB = MockParserA();
 
-        expect(a.props, equals(b.props));
-        expect(a, equals(b));
+      test('2 objects with same props are equal', () {
+        expect(tParserA.props, equals(tParserB.props));
+        expect(tParserA, equals(tParserB));
+      });
+    });
+
+    group('recoverUser', () {
+      const tParser = MockParserA();
+      const tUriString = '';
+
+      test('throws a UrlParseException if no valid url is found', () {
+        expect(
+          () => tParser.recoverUser(tUriString),
+          throwsA(isA<UrlParseException>()),
+        );
       });
     });
   });
