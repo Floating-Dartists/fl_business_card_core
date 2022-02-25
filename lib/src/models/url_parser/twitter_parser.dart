24d6c6b233430243e623ecd8f45d93e0f3fc0490
MeixDev <main.meixdev@gmail.com>
2022-02-26 00:03:10 +0100

    Testing // coverage:ignore-start & ignore-end
    https://github.com/dart-lang/coverage/pull/302

--- lib/src/models/url_parser/twitter_parser.dart
+++ lib/src/models/url_parser/twitter_parser.dart
@@ -2,8 +2,9 @@ import 'url_parser.dart';
 
 class TwitterParser extends UrlParser {
   const TwitterParser()
-      // coverage:ignore-line
+      // coverage:ignore-start
       : super(
+          // coverage:ignore-end
           service: "Twitter",
           hosts: const ["twitter.com", "www.twitter.com"],
           pathSegments: const ["{user}"],
